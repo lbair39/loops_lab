3b386a3663fd2e5e298fae9f37407535e9ca6be1
lbair39 <lilybair24@g.crookcountyschools.org>
2021-11-04 10:08:59 -0700

    worked on my JS

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('I am working')
+
+let i = 0;
+
+const period = ["code", "english", "spanish", "math", "WS"];
+
+while(period[i]){
+console.log(period[i]);
+i++;
+}
+
+while(i<=15){
+    console.log(i);
+    i++;
+  }
+
+
+  for(i=0; i<=5; i=i+1) {
+    console.log(i*5)
+}
